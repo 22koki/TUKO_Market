6335d406162ab20f2086ec974ba9c3dc1a39b4e8
wahome <144784453+22koki@users.noreply.github.com>
2026-09-24 11:27:59 +0300

    feat: expose vendor orders

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import OrderListCreateView
-
+from .views import OrderListCreateView, VendorOrderListView
 urlpatterns = [
     path("", OrderListCreateView.as_view(), name="order-list-create"),
+    path("vendor/", VendorOrderListView.as_view(), name="vendor-order-list"),
 ]
