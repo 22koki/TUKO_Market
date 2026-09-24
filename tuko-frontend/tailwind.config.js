f5fb953fcfafd6d0707210e4dd58b435a36ca231
wahome <144784453+22koki@users.noreply.github.com>
2026-09-24 11:11:59 +0300

    fix: enable class dark mode for CRA

--- tuko-frontend/tailwind.config.js
+++ tuko-frontend/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: "class",
   content: [
-    "./index.html",
+    "./public/index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
