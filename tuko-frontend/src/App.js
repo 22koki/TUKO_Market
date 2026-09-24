29ed7525833e98c8bf0308836fd83c54167dee66
wahome <144784453+22koki@users.noreply.github.com>
2026-09-24 12:22:41 +0300

    feat: add customer vendor rider routes

--- tuko-frontend/src/App.js
+++ tuko-frontend/src/App.js
@@ -1,4 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Marketplace from "./pages/Marketplace";
+import VendorDashboard from "./pages/VendorDashboard";
+import RiderDashboard from "./pages/RiderDashboard";
 import { ThemeProvider } from "./context/ThemeContext";
 import { CartProvider } from "./context/CartContext";
-export default function App(){return <ThemeProvider><CartProvider><Marketplace/></CartProvider></ThemeProvider>}
+
+export default function App(){
+  return (
+    <ThemeProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Marketplace/>}/>
+            <Route path="/vendor" element={<VendorDashboard/>}/>
+            <Route path="/rider" element={<RiderDashboard/>}/>
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </ThemeProvider>
+  );
+}
