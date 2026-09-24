cbf18d5ac10ea1fa4642d43238fe0e35a9291c08
wahome <144784453+22koki@users.noreply.github.com>
2026-09-24 11:19:28 +0300

    feat: add cart and order workflow

---
+++ backend/orders/views.py
@@ -0,0 +1,21 @@
+from rest_framework import generics
+from rest_framework.exceptions import PermissionDenied
+from .models import Order
+from .serializers import CreateOrderSerializer, OrderSerializer
+
+class OrderListCreateView(generics.ListCreateAPIView):
+    def get_queryset(self):
+        return Order.objects.filter(customer=self.request.user).prefetch_related("items__vendor").order_by("-created_at")
+
+    def get_serializer_class(self):
+        return CreateOrderSerializer if self.request.method == "POST" else OrderSerializer
+
+    def perform_create(self, serializer):
+        if self.request.user.role != "customer":
+            raise PermissionDenied("Only customers can place orders.")
+        self.order = serializer.save()
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        response.data = OrderSerializer(self.order).data
+        return response
